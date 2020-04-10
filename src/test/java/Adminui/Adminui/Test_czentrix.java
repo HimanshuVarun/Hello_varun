b1b601209f204ec4d4f951652358fa5b0d8b8a34
HimanshuVarun <himanshuvarun9756@gmail.com>
2020-04-10 21:45:12 +0530

    first commit

---
+++ src/test/java/Adminui/Adminui/Test_czentrix.java
@@ -0,0 +1,16 @@
+package Adminui.Adminui;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class Test_czentrix 
+{   
+   @BeforeTest
+    public  void before() throws Exception 
+	{
+        System.out.println("hello himanshu");
+	}
+   @Test
+	public void license() throws Exception
+	{
+	  System.out.println("himanshu hello");
+	}
+}
